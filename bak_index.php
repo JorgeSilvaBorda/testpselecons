642cd337c6c513f4eb0ec1e1320e34c6fb5678de
Jorge <jorge.silva8686@gmail.com>
2018-11-18 23:11:52 -0300

    Estructura previa
    Sin desarrollos funcionales

---
+++ bak_index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <title>
+        TestPSelecons
+    </title>
+    <!-- Scripts ---------------------------------------------->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/popper.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <!-- script type="text/javascript" src="js/bootstrap.bundle.js"></script-->
+    <!-- /Scripts --------------------------------------------->
+
+    <!-- Estilos ---------------------------------------------->
+    <link rel="icon" href="img/favicon.ico" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/bootstrap-grid.css" />
+    <link rel="stylesheet" href="css/bootstrap-reboot.css" />
+    <link rel="stylesheet" href="css/style.css" />
+	<!-- /Estilos --------------------------------------------->
+
+	<!-- Meta tags -------------------------------------------->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- /Meta tags ------------------------------------------->
+</head>
