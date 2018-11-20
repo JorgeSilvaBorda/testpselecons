fce605bf8b6f7064273940a3049ebbd5c04812bc
Jorge <jorge.silva8686@gmail.com>
2018-11-20 16:17:11 -0300

    Creación básica de módulos restantes

---
+++ mod/metodos-compra.php
@@ -0,0 +1,44 @@
+<div class="modulo" id="modulo-metodos-compra">
+    <header id="header-metodos-compra" class="header">
+	<div class="overlay"></div>
+	<div class="container">
+	    <div class="description">
+		<h1 class="display-3">Métodos de compra.
+		    <p class="lead">En esta sección encontrará la información sobre los medios a través de los que puede realizar el pago de nuestros productos.
+		    </p>   
+		</h1>
+	    </div>
+	</div>
+    </header>
+    <br />
+    <hr class="separador" />
+    <section>
+	<div class="container">
+	    <ol class="list-group">
+		<li class="list-group-item">
+		    <p><span>1. </span>Completar el formulario con los datos del psicólogo(a) que solicita las herramientas. Si es para una organización, se deben incorporar los datos de la empresa y del psicólogo(a) responsable.</p>
+		</li>
+		<li class="list-group-item">
+		    <p><span>2. </span>Adjuntar el número de Inscripción en el Registro Nacional de Prestadores Individuales de Salud del psicólogo(a) solicitante o responsable.</p>
+		</li>
+		<li class="list-group-item">
+		    <p><span>3. </span>Recibirás al email registrado la cotización de los productos y datos de transferencia.</p>
+		</li>
+		<li class="list-group-item">
+		    <p><span>4. </span>Transferir, depositar o pagar vía WebPay el monto cotizado.</p>
+		</li>
+		<li class="list-group-item">
+		    <p><span>5. </span>Recibida la transferencia o depósito, se despacharán los productos según los tiempos estipulados:</p>
+		    <ul class="list-group">
+			<li class="list-group-item">
+			    Despachos en La Serena y Coquimbo se realizarán el mismo día o al día siguiente.
+			</li>
+			<li class="list-group-item">
+			    Otras regiones del país entregas en un plazo de 24 a 48 hrs. Retiro en sucursal o domicilio.
+			</li>
+		    </ul>
+		</li>
+	    </ol>
+	</div>
+    </section>
+</div>
